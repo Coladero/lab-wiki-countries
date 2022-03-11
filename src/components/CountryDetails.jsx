cea398535140bf0b207609b736ef3cd8555d1b41
juanma <fmjuan89@gmail.com>
2022-03-11 08:00:14 +0000

    DoneJustCss

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -35,8 +35,14 @@ function CountryDetails() {
     <h3>{countryDetail.name.common}</h3>
     <h4>Capital: {countryDetail.capital}</h4>
     <h4>Area: {countryDetail.area}</h4>
-    <div key={countryDetail.alpha3Code}>
-    <Link to={`/countries/${countryDetail.alpha3Code}`}>{countryDetail.borders}</Link>
+    <div key={countryDetail.name.common}>
+    {/* esto funciona pero no esta bien, he intentado hacer un map pero no me ha funcionado. */}
+    <Link to={`/countries/${countryDetail.borders[0]}`}>{countryDetail.borders[0]}</Link>
+    <Link to={`/countries/${countryDetail.borders[1]}`}>{countryDetail.borders[1]}</Link>
+    <Link to={`/countries/${countryDetail.borders[2]}`}>{countryDetail.borders[2]}</Link>
+    <Link to={`/countries/${countryDetail.borders[3]}`}>{countryDetail.borders[3]}</Link>
+    <Link to={`/countries/${countryDetail.borders[4]}`}>{countryDetail.borders[4]}</Link>
+    <Link to={`/countries/${countryDetail.borders[5]}`}>{countryDetail.borders[5]}</Link>
     
     </div>
     {console.log(countryDetail.alpha3Code)}
